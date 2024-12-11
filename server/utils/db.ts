83ab196bc1464b11eda20af8c63c8091f6fd56be
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-12-11 21:10:11 +0530

    Deployment

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,17 +1,25 @@
 import mongoose from 'mongoose';
 require('dotenv').config();
 
-const dbUrl:string = process.env.DATABASE_URL || '';
+const dbUrl: string = process.env.DATABASE_URL || '';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(dbUrl).then((data:any) => {
-            console.log(`Database connected with ${data.connection.host}`)
-        })
-    } catch (error:any) {
-        console.log(error.message);
-        setTimeout(connectDB, 5000);
-    }
-}
+  try {
+    // Configuring Mongoose connection options for enhanced reliability
+    const options = {
+      serverSelectionTimeoutMS: 5000, // Timeout for server selection
+      socketTimeoutMS: 45000, // Socket idle timeout
+      connectTimeoutMS: 10000, // Connection timeout
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+    };
+
+    await mongoose.connect(dbUrl, options);
+    console.log('Database connected successfully!');
+  } catch (error: any) {
+    console.error(`Database connection error: ${error.message}`);
+    // Retry connection after 5 seconds
+    setTimeout(connectDB, 5000);
+  }
+};
 
 export default connectDB;
