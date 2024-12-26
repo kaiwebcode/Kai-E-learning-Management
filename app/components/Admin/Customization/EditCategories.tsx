bb86fbecb8a778db1153f4fa9601c17926050e4c
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-12-26 22:46:02 +0530

    categoreis initial

---
+++ app/components/Admin/Customization/EditCategories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function EditCategories({}: Props) {
+  return (
+    <div className='pl-10'>EditCategories</div>
+  )
+}
+
+export default EditCategories
